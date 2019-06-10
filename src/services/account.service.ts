62697497bd51f1370d3a7b6d67859f215df67f2e
Chatyra <chatyrac@icloud.com>
2019-06-10 16:51:10 -0400

    commit

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class AccountService {
+  public_API = 'http://localhost:8080/accounts'
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+  // get account by id method
+
+  get (id: number): Observable<any>{
+    return this.http.get(`http://localhost:8080/accounts/${id}`);
+  }
+
+  //get all accounts method
+
+  getAll(): Observable<any>{
+    return this.http.get(this.public_API);
+  }
+
+  //create an account method
+
+  save(account: Object): Observable<Object>{
+    return this.http.post(this.public_API, account)
+  }
+
+
 }
+
+
+
+
+//   save(person: Object): Observable<Object> {
+//      return this.http.post(this.public_API, person) 
+//   }
+//   remove(id: number) {
+//     return this.http.delete(`http://localhost:8080/person/${id}`);
+//   }
+//   update(id: number, value: any): Observable<Object> {
+//     return this.http.put(`http://localhost:8080/person/${id}`, value);
+//   }
+// }
