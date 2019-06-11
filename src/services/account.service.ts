dd883c4167950a717ea12c4ea30b1f0f1bac8b18
Chatyra <chatyrac@icloud.com>
2019-06-11 09:18:59 -0400

    up to date

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -24,22 +24,42 @@ export class AccountService {
   //create an account method
 
   save(account: Object): Observable<Object>{
-    return this.http.post(this.public_API, account)
+    return this.http.post(`http://localhost:8080/customers/${id}/accounts`, account)
   }
 
+  //get an account owner method
 
+  getOwner(id: number): Observable<any>{
+    return this.http.get(`http://localhost:8080/accounts/${id}/customer`);
+  }
+
+  //get bills by account method
+
+  getBills(id: number): Observable<any>{
+    return this.http.get(`http://localhost:8080/accounts/${id}/bills`);
+  }
+
+  //get withdrawals by account method
+
+  getWithdrawals(id: number): Observable<any>{
+    return this.http.get(`http://localhost:8080/accounts/${id}/withdrawals`);
+  }
+
+  //get deposits by account method
+  
+  getDeposits(id: number): Observable<any>{
+    return this.http.get(`http://localhost:8080/accounts/${id}/deposits`);
+  }
+
+  //update an account method
+  
+  update(id: number, value: any): Observable<Object> {
+    return this.http.put(`http://localhost:8080/accounts/${id}`, value);
+  }
+
+  //delete an account method
+
+  remove(id: number) {
+    return this.http.delete(`http://localhost:8080/accounts/${id}`);
+  }
 }
-
-
-
-
-//   save(person: Object): Observable<Object> {
-//      return this.http.post(this.public_API, person) 
-//   }
-//   remove(id: number) {
-//     return this.http.delete(`http://localhost:8080/person/${id}`);
-//   }
-//   update(id: number, value: any): Observable<Object> {
-//     return this.http.put(`http://localhost:8080/person/${id}`, value);
-//   }
-// }
