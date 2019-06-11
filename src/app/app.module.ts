435c6c3a3997f036fff619f662f38e76a60e85ff
jjackson053 <ljjackson255@gmail.com>
2019-06-10 20:50:58 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerSignUpComponent } from './customer-sign-up/customer-sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerSignUpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/customer-sign-up/customer-sign-up.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/Customer';
+import { CustomerService } from '../../services/customer.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-customer-sign-up',
+  templateUrl: './customer-sign-up.component.html',
+  styleUrls: ['./customer-sign-up.component.css']
+})
+export class CustomerSignUpComponent implements OnInit {
+
+  onSubmit(){
+
+  }
+
+  
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+  }
+
+}
