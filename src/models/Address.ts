615032b44e8843ac02c811677bee3ac24e5979bf
Alberto Rodriguez <ararod9@gmail.com>
2019-06-12 14:09:57 -0400

    updated models

---
+++ src/models/Address.ts
@@ -0,0 +1,10 @@
+
+export class address{
+
+    address_id: number;
+    street_number: string;
+    street_name: string;
+    city: string;
+    state: string;
+    zip: string;
+}
---
+++ src/models/Customer.ts
@@ -0,0 +1,8 @@
+import{ address } from 'src/models/Address';
+
+export class customer{
+    id: number;
+    first_name: string;
+    last_name: string;
+    address: address;
+}
