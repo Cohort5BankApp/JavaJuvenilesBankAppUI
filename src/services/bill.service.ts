ed5c0201498f0e942ebefb60db9efb3637628dd9
Avonte Cannon <acannon007@my.wilmu.edu>
2019-06-12 13:30:18 -0400

    updated Bill Service and Componet

--- src/services/bill.service.ts
+++ src/services/bill.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Bill } from '../models/Bill';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
 
-  constructor() { } 
+  private billUrl: string;
+
+  constructor(private http: HttpClient) { 
+    this.billUrl = 'http://localhost:8080/bill/';
+  } 
+
+  public findAll(): Observable<Bill[]>{
+    return this.http.get<Bill[]>(this.billUrl);
+  }
+
+  public find(id: number){
+    return this.http.get(this.billUrl + id);
+  }
+
+  public create(bill: Bill): Observable<Bill>{
+    return this.http.post<Bill>(this.billUrl, bill);
+  }
+
+  public delete(id: number): Observable<any>{
+    return this.http.delete<Bill>(this.billUrl + id);
+  }
+
+  public update(id: number, value: any){
+    return this.http.put(this.billUrl + id, value);
+  }
 }
