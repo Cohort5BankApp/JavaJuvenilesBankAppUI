7d50ec7beb67f382ea84823f3856056a001a7750
Chatyra <chatyrac@icloud.com>
2019-06-13 12:06:22 -0400

    component

--- src/app/account-details/account-details.component.ts
+++ src/app/account-details/account-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/services/account.service'
+import { Account } from 'src/models/Account'
+import { Deposit } from 'src/models/Deposit'
+import { Withdrawal } from 'src/models/Withdraw'
 
 
 @Component({
@@ -7,15 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-details.component.css']
 })
 export class AccountDetailsComponent implements OnInit {
+account: Account;
+deposits: Array<Deposit>;
+withdrawals: Array<Withdrawal>;
+account_id: number;
 
 
-
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-  
+   this.accountService.get(this.account_id).subscribe(data => {
+     this.account = data;
+   })
+   this.accountService.getDeposits(this.account_id).subscribe(data  => {
+     this.account = data;
+   })
+   this.accountService.getWithdrawals(this.account_id).subscribe(data =>{
+    this.account = data;
+   })
   }
 
-  //sign out button
+  
 
 }
+
