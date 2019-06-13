adf42b5f49a96de74ed4696b5b4012305250958f
T'Shay Brooks <tshayb1209@gmail.com>
2019-06-13 12:24:37 -0400

    first commit

--- src/app/create-withdrawal/create-withdrawal.component.ts
+++ src/app/create-withdrawal/create-withdrawal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Withdrawal } from 'src/models/Withdraw';
+import { WithdrawalService } from 'src/services/withdraw.service';
+import { Subscription, from } from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-withdrawal',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-withdrawal.component.css']
 })
 export class CreateWithdrawalComponent implements OnInit {
+  getWithdrawals: any;
 
-  constructor() { }
 
-  ngOnInit() {
+
+withdrawal: Withdrawal = {id: 0, type: '', transaction_date: '', status: '', account_id: 0, medium: '', amount: 0, description: ''}
+sub: Subscription;
+  
+ 
+
+constructor(private route: ActivatedRoute, private router: Router, private withdrawalService: WithdrawalService) {}
+
+save() {
+    return this.withdrawalService.save(this.withdrawal).subscribe(result => {
+      this.gotoAccountDetails();
+    });
+  }
+delete() {
+    return this.withdrawalService.delete(this.withdrawal.id).subscribe(result => {
+      this.gotoAccountDetails();
+    });
+  }
+find() {
+    return this.withdrawalService.find(this.withdrawal.id).subscribe(result => {
+      this.gotoAccountDetails();
+    });
   }
 
+gotoAccountDetails() {
+  this.router.navigate(['/accounts']);
 }
+
+ngOnInit(){
+  this.sub = this.route.params.subscribe(params => {
+    const id = params['id'];
+    if (id) {
+      this.withdrawalService.find(id).subscribe((withdrawal: Withdrawal) =>{
+        if(withdrawal){
+          this.withdrawal = withdrawal;
+        } else {
+          console.log(`Withdrawal not found with id, '${id}' returning to details`);
+          this.gotoAccountDetails();
+        }
+      })
+    }
+  })
+}
+}
+
