fac74598f2b7a08522307dbbdb8cb9a3b5964f3e
Taylor Brown <brownltaylor@gmail.com>
2019-06-13 11:59:38 -0400

    adding app-routing and app-module

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateAccountComponent } from 'src/app/create-account/create-account.component';
+import { CreateBillComponent } from 'src/app/create-bill/create-bill.component';
+import { CreateWithdrawalComponent } from 'src/app/create-withdrawal/create-withdrawal.component'; 
+import { CreateDepositComponent } from 'src/app/create-deposit/create-deposit.component';
+import { CustomerSignUpComponent } from 'src/app/customer-sign-up/customer-sign-up.component';
+import { CustomerProfileComponent } from 'src/app/customer-profile/customer-profile.component'; 
+import { AccountDetailsComponent } from 'src/app/account-details/account-details.component';
+
+const routes: Routes = [
+{path: '', redirectTo: '/:customer-id/customer-profile', pathMatch: 'full'},
+{path: ':customer_id/create-account', component: CreateAccountComponent},
+{path: ':account_id/update-account', component: CreateAccountComponent},
+{path: ':account_id/create-bill', component: CreateBillComponent},
+{path: ':account_id/update-bill', component: CreateBillComponent},
+{path: ':account_id/create-withdrawal', component: CreateWithdrawalComponent},
+{path: ':account_id/update-withdrawal', component: CreateWithdrawalComponent},
+{path: ':account_id/create-deposit', component: CreateDepositComponent},
+{path: ':account-id/update-deposit', component: CreateDepositComponent},
+{path: 'create-customer', component: CustomerSignUpComponent},
+{path: ':customer-id/update-customer', component: CustomerSignUpComponent},
+{path: ':customer_id/profile', component: CustomerProfileComponent},
+{path: ':account_id', component: AccountDetailsComponent},
+
+
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { CreateAccountComponent } from './create-account/create-account.component';
-=======
-import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
+import { CreateBillComponent } from './create-bill/create-bill.component'; 
+import { CreateWithdrawalComponent } from './create-withdrawal/create-withdrawal.component'; 
 import { CreateDepositComponent } from './create-deposit/create-deposit.component';
 import { CustomerSignUpComponent } from './customer-sign-up/customer-sign-up.component';
-import { CreateBillComponent } from './create-bill/create-bill.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
+import { CustomerProfileComponent } from './customer-profile/customer-profile.component'; 
 import { AccountDetailsComponent } from './account-details/account-details.component';
-import { CreateWithdrawalComponent } from './create-withdrawal/create-withdrawal.component';
->>>>>>> a2ca64185aac6e37452af3bbbe8076ffe3b1ed71
+import { AccountService } from 'src/services/account.service';
+import { BillService } from 'src/services/bill.service';
+import { CustomerService } from 'src/services/customer.service';
+import { DepositService } from 'src/services/deposit.service';
+import { WithdrawalService } from 'src/services/withdraw.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CreateAccountComponent
-=======
-    CustomerProfileComponent,
+    CreateAccountComponent,
+    CreateWithdrawalComponent,
     CreateDepositComponent,
     CustomerSignUpComponent,
     CreateBillComponent,
-    CreateAccountComponent,
-    AccountDetailsComponent,
-    CreateWithdrawalComponent
->>>>>>> a2ca64185aac6e37452af3bbbe8076ffe3b1ed71
+    CustomerProfileComponent,
+    AccountDetailsComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, 
+    AppRoutingModule
+
   ],
-  providers: [],
+  providers: [
+    AccountService, 
+    BillService, 
+    CustomerService, 
+    DepositService, 
+    WithdrawalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
