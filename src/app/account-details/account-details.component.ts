b3caf627e3e7c49ca034a42ccd58fd55e15ad771
Chatyra <chatyrac@icloud.com>
2019-06-13 10:18:35 -0400

    nothing

--- src/app/account-details/account-details.component.ts
+++ src/app/account-details/account-details.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/services/account.service'
 import { Account } from 'src/models/Account'
+import { DepositService } from 'src/services/deposit.service'
+import { Deposit } from 'src/models/Deposit'
+import { Withdrawal } from 'src/models/Withdraw'
+import { WithdrawalService } from 'src/services/withdraw.service'
 
 @Component({
   selector: 'app-account-details',
@@ -9,7 +13,11 @@ import { Account } from 'src/models/Account'
 })
 export class AccountDetailsComponent implements OnInit {
 account: Account;
+deposit: Deposit;
+withdrawal: Withdrawal;
 account_id;
+payee_id;
+
 
   constructor(private accountService: AccountService) { }
 
@@ -17,8 +25,14 @@ account_id;
    this.accountService.get(this.account_id).subscribe(data => {
      this.account = data;
    })
+   this.accountService.getDeposits(this.account_id).subscribe(data  => {
+     this.account = data;
+   })
+   this.accountService.getWithdrawals(this.account_id).subscribe(data =>{
+    this.account = data;
+   })
   }
 
-  //sign out button, deposits and withdrawals on init
+  //sign out button
 
 }
