578863c62b946049024e2885ba4ca460e7a082d5
christopherEdge <edgetopher@gmail.com>
2019-06-14 09:47:28 -0400

    Completed controller

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from 'src/services/account.service';
 import { FormsModule } from '@angular/forms';
+import { Message } from 'src/models/Message';
+import {Account } from 'src/models/Account';
 
 
 @Component({
@@ -13,15 +15,17 @@ export class CreateAccountComponent implements OnInit {
 
   type:string[] =["Savings","Checking","Credit"];
   Account: Account;
+  accounts:Account ={account_id: 0, nickname:"", rewards:0,balance:0,customer_id:0,type:""};
   constructor(private route:ActivatedRoute, private router:Router, private service:AccountService) {
-    console.log(service.getAll().subscribe());
    }
 
   ngOnInit() {
+  this.service.getAll().subscribe(data => 
+    console.log(data))
   }
   onSubmit(){
-    let id :number = +this.Account.id;
-    this.service.save(this.Account, id).subscribe(result =>{
+    let id :number = +this.accounts.account_id;
+    this.service.save(this.accounts, id).subscribe(result =>{
       this.gotoAccountList();
     }, error => console.error(error));
     console.log(this.Account);
