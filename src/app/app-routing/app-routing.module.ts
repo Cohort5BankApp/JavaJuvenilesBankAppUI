476b615b0593766a2644be9c15b0f7926d801924
billycropper <williamcropper@Billys-MBP.local>
2019-06-14 01:42:51 -0400

    updates

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -10,18 +10,18 @@ import { AccountDetailsComponent } from 'src/app/account-details/account-details
 import { TestComponent } from '../test/test.component';
 
 const routes: Routes = [
-// {path: ':customer_id/create-account', component: CreateAccountComponent},
-// {path: ':account_id/update-account', component: CreateAccountComponent},
-// {path: ':account_id/create-bill', component: CreateBillComponent},
-// {path: ':account_id/update-bill', component: CreateBillComponent},
-// {path: ':account_id/create-withdrawal', component: CreateWithdrawalComponent},
-// {path: ':account_id/update-withdrawal', component: CreateWithdrawalComponent},
-// {path: ':account_id/create-deposit', component: CreateDepositComponent},
-// {path: ':account-id/update-deposit', component: CreateDepositComponent},
-{path: 'test', component: CustomerSignUpComponent}
-// {path: ':customer-id/update-customer', component: CustomerSignUpComponent},
-// {path: ':customer_id/profile', component: CustomerProfileComponent},
-// {path: ':customer_id/accounts/:account_id', component: AccountDetailsComponent},
+{path: ':customer_id/create-account', component: CreateAccountComponent},
+{path: ':account_id/update-account', component: CreateAccountComponent},
+{path: ':account_id/create-bill', component: CreateBillComponent},
+{path: ':account_id/update-bill', component: CreateBillComponent},
+{path: ':account_id/create-withdrawal', component: CreateWithdrawalComponent},
+{path: ':account_id/update-withdrawal', component: CreateWithdrawalComponent},
+{path: ':account_id/create-deposit', component: CreateDepositComponent},
+{path: ':account-id/update-deposit', component: CreateDepositComponent},
+{path: 'create-customer', component: CustomerSignUpComponent},
+{path: ':customer-id/update-customer', component: CustomerSignUpComponent},
+{path: ':customer_id/profile', component: CustomerProfileComponent},
+{path: ':customer_id/accounts/:account_id', component: AccountDetailsComponent},
 
 
 
