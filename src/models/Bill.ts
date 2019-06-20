2f0704eab857c4998dbd8b4b78ecd05a4038cb22
jjackson053 <ljjackson255@gmail.com>
2019-06-20 10:26:47 -0400

    update

--- src/models/Bill.ts
+++ src/models/Bill.ts
@@ -6,8 +6,8 @@ export class Bill{
     nickname : string;
     creation_date : string;
     payment_date : string;
-    recurring_date : string;
+    recurring_date : number;
     upcoming_payment_date : string;
-    payment_amount : string;
+    payment_amount : number;
     account_id : number;
 }
