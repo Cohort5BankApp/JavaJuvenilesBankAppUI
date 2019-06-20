c7cd791b21b8e8c8fb166a5ff61920badaab086f
christopherEdge <edgetopher@gmail.com>
2019-06-20 09:44:40 -0400

    routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -14,21 +14,21 @@ const routes: Routes = [
 {path: ':customer_id/create-account', component: CreateAccountComponent},
 {path: ':account_id/update-account', component: CreateAccountComponent},
 {path: ':account_id/create-bill', component: CreateBillComponent},
-{path: ':account_id/update-bill', component: CreateBillComponent},
+{path: ':account_id/update-bill/:id', component: CreateBillComponent},
 {path: ':account_id/create-withdrawal', component: CreateWithdrawalComponent},
-{path: ':account_id/update-withdrawal', component: CreateWithdrawalComponent},
+{path: ':account_id/update-withdrawal/:id', component: CreateWithdrawalComponent},
 {path: ':account_id/create-deposit', component: CreateDepositComponent},
-{path: ':account-id/update-deposit', component: CreateDepositComponent},
-{path: 'test', component: CustomerSignUpComponent},
+{path: ':account-id/update-deposit/:id', component: CreateDepositComponent},
+{path: 'signup', component: CustomerSignUpComponent},
 {path: ':customer-id/update-customer', component: CustomerSignUpComponent},
 {path: ':customer_id/profile', component: CustomerProfileComponent},
 {path: ':customer_id/accounts/:account_id', component: AccountDetailsComponent},
 {path: 'customers', component: CustomerListComponent},
-{path: 'update-bill/:id', component: CreateBillComponent},
-{path: 'update-deposit/:id', component: CreateDepositComponent},
-{path: 'update-withdrawal/:id', component: CreateWithdrawalComponent},
-{path: 'update-account/:account_id', component: CreateAccountComponent},
-{path: 'update-customer/:customer_id', component: CustomerSignUpComponent}
+// {path: 'update-bill/:id', component: CreateBillComponent},
+// {path: 'update-deposit/:id', component: CreateDepositComponent},
+// {path: 'update-withdrawal/:id', component: CreateWithdrawalComponent},
+// {path: 'update-account/:account_id', component: CreateAccountComponent},
+// {path: 'update-customer/:customer_id', component: CustomerSignUpComponent}
 
 
 
