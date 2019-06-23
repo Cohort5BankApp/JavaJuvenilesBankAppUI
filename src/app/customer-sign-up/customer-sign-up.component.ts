5b759d5b4777592e6925744e273586d190f4bbed
jjackson053 <ljjackson255@gmail.com>
2019-06-23 13:21:29 -0400

    update

--- src/app/customer-sign-up/customer-sign-up.component.ts
+++ src/app/customer-sign-up/customer-sign-up.component.ts
@@ -32,6 +32,7 @@ sub: Subscription;
   }
 
   onSubmit(){
+    console.log(this.customer);
     this.addresses = [this.address];
     this.customer.addresses = this.addresses;
 this.customerService.create(this.customer).subscribe(result => {this.goToCustomerProfile()});
